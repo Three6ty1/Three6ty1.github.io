2fcb071b0382de6c457b5f73868b11b759861825
W Z <wk.zhang02@gmail.com>
2023-05-26 01:34:25 +1000

    Added react-scroll functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,19 @@ import Projects from './Projects';
 import Contact from './Contact';
 import Interests from './Interests';
 
+/*
+TODO: On hover and backwards effect for DP
+TODO: Mobile responsiveness (Maybe)
+TODO: Add proper images
+TODO: Add wavey celeste like bobbing to my name text
+TODO: Add flair to the plain background 
+TODO: Set up actual links for the project (make it bob?) and contact section
+
+*/
+
 function Home() {
   return (
-    <section id='home-section'>
+    <section id='Home'>
       <About />
       <Contact />
       <br />
