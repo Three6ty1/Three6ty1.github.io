d5c5246672132449cc5c28c2b60a305cd1d0c20f
Three6ty1 <wk.zhang02@gmail.com>
2023-05-28 23:15:23 +1000

    Added wavy celeste bobbing to name text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,15 @@ function About() {
       <br />
       <div className='about-info'>
         <Typography display='inline' variant='h3'>Hello! I'm </Typography>
-        <Typography sx={{color: '#1BA098'}} display='inline' variant='h3'>Wen Kai</Typography>
+        <Typography className='moving-name' display='inline' variant='h3'>
+          <span style={{'--n':0}}>W</span> 
+          <span style={{'--n':1}}>e</span>
+          <span style={{'--n':2}}>n</span>
+          <span style={{'--n':3}}> </span>
+          <span style={{'--n':4}}>K</span>
+          <span style={{'--n':5}}>a</span>
+          <span style={{'--n':6}}>i</span>
+        </Typography>
         <Typography display='inline' variant='h3'> Zhang</Typography>
       </div>
       <br />
@@ -27,6 +35,7 @@ function About() {
       <br />
       <div className='about-info'>
         <Typography display="block" variant='h5'>I'm a 4th year Computer Science student studying at UNSW</Typography>
+        <Typography display="block" variant='body1'>P.S. My name does a little animation on hover!</Typography>
       </div>
     </section>
   )
